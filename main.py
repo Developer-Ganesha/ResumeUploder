192f52b484ef7036420d942b75e4d6c2b8671d6c
Developer-Ganesha <ganeshsavner320@gmail.com>
2025-03-12 14:43:27 +0530

    added the port

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ async def registration(
 ):
     if Email in users_db:
         raise HTTPException(status_code=400, detail="Email already registered")
+    
     users_db[Email] = {
         "Full_Name": Full_Name,
         "Date_of_Birth": Date_of_Birth,
@@ -73,3 +74,8 @@ async def registration(
         )
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error uploading resume: {str(e)}")
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app,host="0.0.0.0.",port=8000 ,log_level = "debug")
+
