e723a836a7aedd4ebcaa1e9fafae5c81f341213c
Developer-Ganesha <144231101+Developer-Ganesha@users.noreply.github.com>
2025-04-30 11:07:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -163,4 +163,4 @@ def delete_user(id:int = Form(...),db:Session = Depends(get_db)):
            
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
+    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="debug")
